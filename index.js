8c5824e4fd5dcf7d511e5eb975c61b26912bbe27
neyaznafiz <niiyazobug@gmail.com>
2022-12-26 18:39:27 +0600

    routes created

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRouter from "./routes/auth.js";
+import usersRouter from "./routes/users.js";
+import hotelsRouter from "./routes/hotels.js";
+import roomsRouter from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -8,21 +12,31 @@ dotenv.config();
 const connect = async () => {
   try {
     // mongoose
-      await mongoose.connect(process.env.MONGO);
-      console.log("Connected to MongoDB");
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
   } catch (error) {
     throw error;
   }
 };
 
-mongoose.connection.on("disconnected", () => { 
-    console.log("MongoDB disconnected..!");
-})
-mongoose.connection.on("dconnected", () => { 
-    console.log("MongoDB connected..");
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected..!");
+});
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected..");
+});
+
+// middleware
+app.use("/api/auth", authRouter);
+app.use("/api/users", authRouter);
+app.use("/api/hotels", authRouter);
+app.use("/api/rooms", authRouter);
+
+app.get("/", (req, res) => {
+  res.send("BookingVerse Server is running");
+});
 
 app.listen(5000, () => {
-    connect()
+  connect();
   console.log("Connect to backend..!");
 });
---
+++ routes/hotels.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+// create
+router.post("/", (req, res) => { 
+    
+})
+
+// update
+// get
+// get all
+
+export default router;
